779dc75bb1072c7d16e3174a8c701d9256b11347
Vsevolod Zorin <vsevolod.dev@gmail.com>
2023-04-26 11:16:13 +0300

    add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TelegramModule } from './services/telegram/telegram.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getTelegramConfig } from './config/telegram.config';
+import { TelegramModule } from './services/telegram/telegram.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TelegramModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getTelegramConfig,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
