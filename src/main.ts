125b06d2c28c931536e6adc7cc3a9b92f0c2441f
Vsevolod Zorin <vsevolod.dev@gmail.com>
2023-04-26 11:32:28 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: 'true' });
+
+  const config = new DocumentBuilder()
+    .setTitle('project-dev-way-auth')
+    .setDescription('project-dev-way-auth')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
